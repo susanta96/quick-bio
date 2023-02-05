3eb44cf5879530ca0502dfd20b2776ea83bc58dc
susanta96 <svchost96@gmail.com>
2023-02-05 15:21:29 +0530

    initial project setup and openAI integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,25 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Generate your next Social Media bio in seconds."
+          />
+          <meta property="og:site_name" content="quick-bio.asdevs.dev" />
+          <meta
+            property="og:description"
+            content="Generate your next Social Media bio in seconds."
+          />
+          <meta property="og:title" content="Social Bio Generator" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Social Bio Generator" />
+          <meta
+            name="twitter:description"
+            content="Generate your next Social Media bio in seconds."
+          />
+        </Head>
       <body>
         <Main />
         <NextScript />
